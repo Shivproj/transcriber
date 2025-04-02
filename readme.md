930701edfc576385d72c6f0d1a8a4dbe2848d3cc
Shivproj <shivindrakanti@gmail.com>
2025-04-02 09:41:41 +0530

    code change

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 Hi there here is the transcriber proj will add stuff later
-Run this using fastapi dev main.py
+* Run this using `fastapi dev main.py`
 and acccess localhost/docs
+* Alt - use `uvicorn src.main:app --reload` 
--- src/main.py
+++ src/main.py
@@ -4,9 +4,8 @@ import aiofiles
 
 app = FastAPI()
 
-# Set a default directory if the environment variable is not set
+
 UPLOAD_DIRECTORY = os.environ.get("Upload_dir", "uploaded_files")
-os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
 
 @app.get("/")
 def read_root():
@@ -17,11 +16,14 @@ async def upload_file(file: UploadFile = File(...)):
     if not file:
         raise HTTPException(status_code=400, detail="No file uploaded")
     
+    if not os.path.exists(UPLOAD_DIRECTORY):
+        os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
+    
     file_location = os.path.join(UPLOAD_DIRECTORY, file.filename)
     try:
         async with aiofiles.open(file_location, "wb") as f:
-            content = await file.read()  # Read the file content
-            await f.write(content)  # Write the file content to the destination
+            content = await file.read()
+            await f.write(content)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"File upload failed: {str(e)}")
     
